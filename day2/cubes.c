7dc6354401284eabeb60aa86545016d8c01f2d31
Brian Guerrero <BGuerrero825@gmail.com>
2023-12-06 22:45:58 -0600

    day 2 solution, in C!
    reminded myself why strings are a pain in the arse with standard library C but learned a lot along the way. Also rolled some of my own string parsing functions to make life easier going forward :)

--- day2/cubes.c
+++ day2/cubes.c
@@ -4,33 +4,81 @@
 
 #define INPUT_FILEPATH "C:\\Users\\brian\\Dev\\adventofcode23\\day2\\input.txt"
 const size_t LINE_SIZE = 1000;
+const int GREEN_MAX = 13;
+const int BLUE_MAX =  14;
+const int RED_MAX = 12;
+
+int check_game(char * line);
+int startswith(const char * prefix, const char * token);
+int endswith(const char * suffix, const char * token);
+void slice(char* token, char* segment, size_t start, size_t end);
+
 
 int main(){
-	// open a file
+	// open a file to read
 	FILE * fp;
 	fp = fopen(INPUT_FILEPATH, "r");
 	// give an error if file doesn't exist
 	if(!fp){
 		perror(INPUT_FILEPATH);
 	}
-	// get each line of the file while null not returned (EOF)
+	// get each line of the file while null not returned (EOF) and increment a sum using check_game()
 	char line[LINE_SIZE];
+	size_t sum = 0;
 	while(fgets(line, LINE_SIZE, fp) != NULL){
-		printf("%s -------- ", line);
-		check_game(line);
-		printf("\n");
+		printf("\n%s", line);
+		sum += check_game(line);
 	}
+	printf("\n\n--- GAMES SUM --- : %d\n", sum);
 	fclose(fp);
 }
 
 int check_game(char * line){
-	int green_count, blue_count, red_count = 0;
-	char * token = strtok(line, " ");
-	char * prev_token;
-	int game_num = 0;
+	// initialize token pointers, splitting on whitespace
+	char * prev_token = strtok(line, " ");
+	char * token = strtok(NULL, " ");
+	// parse game number from the 2nd token
+	char * game_str;
+	slice(token, game_str, 0, strlen(token)-1);
+	int game_num = atoi(game_str);
+	// iterate tokens in line
 	while(token != NULL){
-		printf("%s\n", token);
+		// printf("%s | ", token);
+		//check if any colors drawn were over their respective max size, jump to oversize label if so
+		if(startswith("green", token)){
+			if(atoi(prev_token) > GREEN_MAX){
+				goto oversize;
+			}
+		}
+		else if(startswith("blue", token)){
+			if(atoi(prev_token) > BLUE_MAX){
+				goto oversize;
+			}
+		}
+		else if(startswith("red", token)){
+			if(atoi(prev_token) > RED_MAX){
+				goto oversize;
+			}
+		}
+		// update tokens in line
 		prev_token = token; 	
 		token = strtok(NULL, " ");
 	}
+	return game_num;
+	// label to exit out of oversize games 
+	oversize:
+		printf("OVERSIZE ---- color: %s amount: %s\n", token, prev_token);
+		return 0;
+}
+
+int startswith(const char * prefix, const char * token){
+	return strncmp(prefix, token, strlen(prefix)) == 0;
+}
+
+int endswith(const char * suffix, const char * token){
+	return strncmp(suffix, token + (strlen(token) - strlen(suffix)), strlen(suffix)) == 0;
+}
+
+void slice(char* token, char* segment, size_t start, size_t end){
+    strncpy(segment, token + start, end - start);
 }
